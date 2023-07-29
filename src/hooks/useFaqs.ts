db8692482c2cc09e3300c22ef869f38f799ee39c
Caleb <calebwagner5@gmail.com>
2023-07-29 08:10:45 -0500

    cleaning up repo

--- src/hooks/useFaqs.ts
+++ src/hooks/useFaqs.ts
@@ -2,15 +2,11 @@ import { useState } from "react";
 import { GENERAL_QUESTIONS_TOPIC_ID } from "../constants/Constants";
 import data from "../data/faqs.json";
 
-type Faq = {
-  id: number;
-  title: string;
-  topicId: number;
-  question: string;
-  answer: string;
-  sortOrder: number;
-  category: string | null;
-};
+/**
+ * useFaqs hook manages an array of FAQs filtered by their topic ID.
+ * It returns the filtered FAQs based on the active topic, and a function
+ * that allows changing the active topic ID.
+ */
 
 export const useFaqs = (
   defaultTopicId: number = GENERAL_QUESTIONS_TOPIC_ID
